63a24faac22e8678b377feb65b3ea38a326c8d33
Ruben Carvalho <33361409+Arukiap@users.noreply.github.com>
2019-05-31 21:33:15 +0100

    Add edge case for mandelbrot iterative function
    Also change orbit trap to 2 dimensions

--- shaders/myShader.fs
+++ shaders/myShader.fs
@@ -9,9 +9,9 @@ in vec4 gl_FragCoord;
 varying float vSystemTime;
 varying vec2 vSystemResolution;
 
-float maxIterations = 70;
+float maxIterations = 100;
 
-float orbitTrap = 2; //Max length is always 2
+vec2 orbitTrap = vec2(2.0,2.0); //Max length is always 2
 
 // (x+yi)(x+yi) = x^2 + 2xyi - y^2
 // imaginary numbers represented by vec2(real,imaginary)
@@ -26,14 +26,15 @@ float iterateMandebrot(vec2 coord){
 	vec2 z = vec2(0,0);
 	for(int i=0;i<maxIterations;i++){
 		z = squareImaginary(z) + coord;
-		orbitTrap = min(orbitTrap,length(z));
+		orbitTrap = min(orbitTrap,abs(z));
 		if(length(z)>2) return i/maxIterations;
 	}
+	return maxIterations;
 }
 
 void main(){	
 	vec2 teste = gl_FragCoord.xy/vSystemResolution;
 	teste.x = teste.x*4-2;
 	teste.y = teste.y*4-2;
-	gl_FragColor = vec4(orbitTrap,0.2+orbitTrap/2,1.0,1.0)*iterateMandebrot(teste);
+	gl_FragColor = vec4(iterateMandebrot(teste));
 }
